14aff6601d97c1bf15a6c25e2c9cef0e758c5a12
Alexandr Zemlyanskiy <sasha.zemlyanskiy.00@mail.ru>
2022-03-14 21:08:04 +0300

    add shell blog

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Blog.urls'
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "Blog/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             'context_processors': [
